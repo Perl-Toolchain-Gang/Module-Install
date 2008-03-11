0b5c998cfe0b999b023ae5cc84b8a63e8cb18cfd
Adam Kennedy <adam@ali.as>
2008-03-11 05:10:09 +0000

    Deprecating auto_install, it's just too dangerous

--- lib/Module/Install/AutoInstall.pm
+++ lib/Module/Install/AutoInstall.pm
@@ -1,6 +1,7 @@
 package Module::Install::AutoInstall;
 
 use strict;
+use Carp ();
 use Module::Install::Base;
 
 use vars qw{$VERSION $ISCORE @ISA};
@@ -10,51 +11,24 @@ BEGIN {
 	@ISA     = qw{Module::Install::Base};
 }
 
-sub AutoInstall { $_[0] }
+sub AutoInstall {
+    Carp::croak('Module::Install::AutoInstall often breaks CPAN and has been deprecated');
+}
 
 sub run {
-    my $self = shift;
-    $self->auto_install_now(@_);
+    Carp::croak('Module::Install::AutoInstall often breaks CPAN and has been deprecated');
 }
 
 sub write {
-    my $self = shift;
-    $self->auto_install(@_);
+    Carp::croak('Module::Install::AutoInstall often breaks CPAN and has been deprecated');
 }
 
 sub auto_install {
-    my $self = shift;
-    return if $self->{done}++;
-
-    # Flatten array of arrays into a single array
-    my @core = map @$_, map @$_, grep ref,
-               $self->build_requires, $self->requires;
-
-    my @config = @_;
-
-    # We'll need Module::AutoInstall
-    $self->include('Module::AutoInstall');
-    require Module::AutoInstall;
-
-    Module::AutoInstall->import(
-        (@config ? (-config => \@config) : ()),
-        (@core   ? (-core   => \@core)   : ()),
-        $self->features,
-    );
-
-    $self->makemaker_args( Module::AutoInstall::_make_args() );
-
-    my $class = ref($self);
-    $self->postamble(
-        "# --- $class section:\n" .
-        Module::AutoInstall::postamble()
-    );
+    Carp::croak('Module::Install::AutoInstall often breaks CPAN and has been deprecated');
 }
 
 sub auto_install_now {
-    my $self = shift;
-    $self->auto_install(@_);
-    Module::AutoInstall::do_install();
+    Carp::croak('Module::Install::AutoInstall often breaks CPAN and has been deprecated');
 }
 
 1;
