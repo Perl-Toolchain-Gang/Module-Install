75c0d313d783df91240a43cc0920f078cfdc7544
Adam Kennedy <adam@ali.as>
2008-07-17 07:53:10 +0000

    Preparing for release

--- lib/Module/Install/Compiler.pm
+++ lib/Module/Install/Compiler.pm
@@ -11,40 +11,53 @@ BEGIN {
 	@ISA     = qw{Module::Install::Base};
 }
 
+sub ppport {
+	my $self = shift;
+	if ( $self->is_admin ) {
+		return $self->admin->ppport(@_);
+	} else {
+		# Fallback to just a check
+		my $file = shift || 'ppport.h';
+		unless ( -f $file ) {
+			die "Packaging error, $file is missing";
+		}
+	}
+}
+
 sub cc_files {
-    require Config;
-    my $self = shift;
-    $self->makemaker_args(
-        OBJECT => join ' ', map { substr($_, 0, -2) . $Config::Config{_o} } @_
-    );
+	require Config;
+	my $self = shift;
+	$self->makemaker_args(
+		OBJECT => join ' ', map { substr($_, 0, -2) . $Config::Config{_o} } @_
+	);
 }
 
 sub cc_inc_paths {
-    my $self = shift;
-    $self->makemaker_args(
-        INC => join ' ', map { "-I$_" } @_
-    );
+	my $self = shift;
+	$self->makemaker_args(
+		INC => join ' ', map { "-I$_" } @_
+	);
 }
 
 sub cc_lib_paths {
-    my $self = shift;
-    $self->makemaker_args(
-        LIBS => join ' ', map { "-L$_" } @_
-    );
+	my $self = shift;
+	$self->makemaker_args(
+		LIBS => join ' ', map { "-L$_" } @_
+	);
 }
 
 sub cc_lib_links {
-    my $self = shift;
-    $self->makemaker_args(
-        LIBS => join ' ', $self->makemaker_args->{LIBS}, map { "-l$_" } @_
-    );
+	my $self = shift;
+	$self->makemaker_args(
+		LIBS => join ' ', $self->makemaker_args->{LIBS}, map { "-l$_" } @_
+	);
 }
 
 sub cc_optimize_flags {
-    my $self = shift;
-    $self->makemaker_args(
-        OPTIMIZE => join ' ', @_
-    );
+	my $self = shift;
+	$self->makemaker_args(
+		OPTIMIZE => join ' ', @_
+	);
 }
 
 1;
---
+++ t/06_ppport.t
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+# Check that ppport works
+
+use strict;
+use vars qw{$VERSION};
+BEGIN {
+	$|       = 1;
+	$^W      = 1;
+	$VERSION = '0.76';
+}
+
+use Test::More tests => 1;
+
+ok( -f 'ppport.h', 'Found ppport.h' );
