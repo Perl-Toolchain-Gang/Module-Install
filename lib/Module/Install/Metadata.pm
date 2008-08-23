af6dd0234d853e7277d8190a4dca1eff709d261b
Adam Kennedy <adam@ali.as>
2008-08-23 22:49:38 +0000

    Moving install_script back to it's own module, to unbreak people that had upgraded

--- lib/Module/Install/Metadata.pm
+++ lib/Module/Install/Metadata.pm
@@ -481,19 +481,4 @@ sub bugtracker_from {
 	return 1;
 }
 
-sub install_script {
-	my $self = shift;
-	my $args = $self->makemaker_args;
-	my $exe  = $args->{EXE_FILES} ||= [];
-        foreach ( @_ ) {
-		if ( -f $_ ) {
-			push @$exe, $_;
-		} elsif ( -d 'script' and -f "script/$_" ) {
-			push @$exe, "script/$_";
-		} else {
-			die("Cannot find script '$_'");
-		}
-	}
-}
-
 1;
